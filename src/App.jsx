77eeb40c613ca4a689af02f6cf3b10780585c3ee
grp0060078 <grp006007@gmail.com>
2023-10-14 12:38:07 +0530

    names added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-function App() {
-  const now =new Date()
+function Hello(props) {
+  
  
   return (
   
-    <p>hello Anaipatti.It is{now.toString()} </p>
+    <p>hello {props.username}</p>
   )
 }
+function App () {
+  let user1 = 'sathish'
+  let user2 = 'krish'
+  let user3 = 'rithvik'
+  let user4 = 'vijay'
+  return (
+   <div>
+  <Hello username={user1}/>
+  <Hello username={user2}/>
+  <Hello username={user3}/>
+  <Hello username={user4}/>
+  </div>
+    
+  )
+}
+
 export default App;
