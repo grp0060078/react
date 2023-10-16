c851ac7100e88c6dfea59a687a45395b4f874fa5
grp0060078 <grp006007@gmail.com>
2023-10-16 20:14:36 +0530

    session task completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,51 @@
-import React from 'react';
-import './components/App.css';
 
-import PriceCard from './components/Pricecard';
+import React, { Component } from 'react';
 
-function App() {
-  
-  return (
-    
-    <div className="App">
-      <h1>React Price Card Task</h1>
-      <h4>Reproduce this user interface using a React.js application with JSX. 
-        Write the code in a dynamic manner.</h4>
-      
-    
-      <div className="PriceCard">
-      <div className="price-cards">
-    
-        <PriceCard
-        
-          title="FREE"
-          price="0"
-          features={['✅Single User', '✅50GB Storage', '✅Unlimited Public projects','✅Community Access',
-          '❎Unlimited Private Projects','❎Deidcated Phone Support','❎Free Subdomain','❎Monthly Status Reports']}
-          button="Button"
-        />
-        
-        
-        <PriceCard
-          title="PLUS"
-          price="9"
-          features={['✅Single User', '✅50GB Storage', '✅Unlimited Public projects','✅Community Access',
-          '✅Unlimited Private Projects','✅Deidcated Phone Support','✅Free Subdomain','❎Monthly Status Reports']}
-          button="Button"
-       />
-       
-        <PriceCard
-          title="PRO"
-          price="49"
-          features={['✅Single User', '✅50GB Storage', '✅Unlimited Public projects','✅Community Access',
-          '✅Unlimited Private Projects','✅Deidcated Phone Support','✅Free Subdomain','✅Monthly Status Reports']}
-          button="Button"
-       />
-     
-    </div>
-    </div>
-    </div>
-  );
- 
+class App extends Component {
+
+  constructor(props) {
+    super(props); // to call the parent class constructor
+
+    // define the state
+    this.state = {
+      count: 0,
+    }
+  }
+
+  increment = () => {
+    // update the state (data) of the count
+    this.setState({
+      // provide the new state
+      count: this.state.count + 1,
+    })
+  }
+
+  decrement = () => {
+    // update the state (data) of the count
+    this.setState({
+      // provide the new state
+      count: this.state.count - 1,
+    })
+  }
+
+  reset = () => {
+    // update the state (data) of the count
+    this.setState({
+      // provide the new state
+      count:0
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Count: { this.state.count }</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>decrement</button>
+        <button onClick={this.reset}>reset</button>
+      </div>
+    )
+  }
 }
 
 export default App;
