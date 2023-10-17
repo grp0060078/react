ff941c92a8b0269a526d37aaf80681e9f2de40eb
grp0060078 <grp006007@gmail.com>
2023-10-17 19:30:36 +0530

    add cart & remove cart

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,33 @@ class App extends Component {
 
     // define the state
     this.state = {
-      count: 0,
+      addcart:false
     }
   }
 
-  increment = () => {
+  removecart = () => {
     // update the state (data) of the count
     this.setState({
       // provide the new state
-      count: this.state.count + 1,
-    })
+      addcart:!this.state.addcart
+      }
+    )
   }
 
-  decrement = () => {
-    // update the state (data) of the count
-    this.setState({
-      // provide the new state
-      count: this.state.count - 1,
-    })
-  }
-
-  reset = () => {
-    // update the state (data) of the count
-    this.setState({
-      // provide the new state
-      count:0
-    })
-  }
+  
+  
 
   render() {
     return (
       <div>
-        <p>Count: { this.state.count }</p>
-        <button onClick={this.increment}>Increment</button>
-        <button onClick={this.decrement}>decrement</button>
-        <button onClick={this.reset}>reset</button>
+        {this.state.addcart ? (
+          <button onClick={this.removecart}>addcart</button>
+        )
+        
+       : (
+        <button onClick={this.removecart}>Removecart</button>
+       )
+        }
       </div>
     )
   }
