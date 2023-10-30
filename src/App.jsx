ea62ddda189d0c273e3d275a54a68e9e74ebe81a
grp0060078 <grp006007@gmail.com>
2023-10-30 20:04:21 +0530

    commit formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,65 @@
+// Formik: small group of react components and hooks for building forms in React
 
-import React, { useState, createContext, } from 'react';
-import Profile from './components/profile.jsx'
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import React from 'react';
+import './styles/App.css';
 
-const ProfileContext = createContext();
-
-
-function App  ()  {
-  
-  const [profileName, setProfileName] = useState('');
-
+function ContactForm() {
   return (
+    <Formik
+      initialValues={{ name: '', email: '', message: '' }}
 
-    <ProfileContext.Provider value={{ profileName, setProfileName }}>
-      <Profile />
-    </ProfileContext.Provider>
-  );
-};
+      validate={(values) => {
+        const errors = {};
 
+        if (!values.name) {
+          errors.name = 'Name is required';
+        }
 
+        if (values.email == '') {
+          errors.email = 'Email is required';
+        } else if(!/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/i.test(values.email)){
+          errors.email = 'Invalid email address';
+        }
 
+        return errors;
+      }}
 
-export { App as default, ProfileContext };
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      <Form>
+        <div>
+          <label htmlFor='name'>Name</label>
+          <Field type='text' name='name' />
+          <ErrorMessage name='name' component='div' className='error'/>
+        </div>
+
+        <div>
+          <label htmlFor='email'>Email</label>
+          <Field type='email' name='email' />
+          <ErrorMessage name='email' component='div' className='error'/>
+        </div>
+
+        <div>
+          <label htmlFor='message'>Message</label>
+          <Field as='textarea' name='message' />
+          <ErrorMessage name='message' component='div' className='error'/>
+        </div>
+
+        <button type='submit'>Submit</button>
+    </Form>
+    </Formik>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  )
+}
+
+export default App;
