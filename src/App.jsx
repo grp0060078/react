688867cafffcc376a3ac67dedc0ff078875457ff
grp0060078 <grp006007@gmail.com>
2023-11-02 10:14:46 +0530

    redux class

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,22 @@
-// Formik: small group of react components and hooks for building forms in React
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 
-import { ErrorMessage, Field, Form, Formik } from 'formik';
-import React from 'react';
-import './styles/App.css';
+const App = () =>{
 
-function ContactForm() {
+  const dispatch=useDispatch();
+  const counter =useSelector((state) =>state)
   return (
-    <Formik
-      initialValues={{ name: '', email: '', message: '' }}
-
-      validate={(values) => {
-        const errors = {};
-
-        if (!values.name) {
-          errors.name = 'Name is required';
-        }
-
-        if (values.email == '') {
-          errors.email = 'Email is required';
-        } else if(!/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/i.test(values.email)){
-          errors.email = 'Invalid email address';
-        }
-
-        return errors;
-      }}
-
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      <Form>
+      <div>
         <div>
-          <label htmlFor='name'>Name</label>
-          <Field type='text' name='name' />
-          <ErrorMessage name='name' component='div' className='error'/>
-        </div>
+        count:{counter}
 
-        <div>
-          <label htmlFor='email'>Email</label>
-          <Field type='email' name='email' />
-          <ErrorMessage name='email' component='div' className='error'/>
+        <button onClick={() =>dispatch({type:"INCR"})}>plus</button>
+        <button onClick={() =>dispatch({type:"DECR"})}>minus</button>
+        <button onClick={() =>dispatch({type:"ZERO"})}>zero</button>
         </div>
-
-        <div>
-          <label htmlFor='message'>Message</label>
-          <Field as='textarea' name='message' />
-          <ErrorMessage name='message' component='div' className='error'/>
-        </div>
-
-        <button type='submit'>Submit</button>
-    </Form>
-    </Formik>
+   </div>
   )
-}
+   
+ }
 
-function App() {
-  return (
-    <div>
-      <ContactForm />
-    </div>
-  )
-}
-
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+   const countReducer =(state=0,action) =>{
+    switch(action.type){
+        case "INCR":
+            return state +1;
+
+        case "DECR":
+            return state -1;
+
+        case "ZERO":
+            return 0;  
+            
+        default:
+            return state;    
+    }
+       
+    
+   }
+   const store = createStore(countReducer);
+   
+   store.subscribe(() => {
+       console.log(store.getState())
+   })
+   
+   
+  
+   
 
 
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
 
 
 
